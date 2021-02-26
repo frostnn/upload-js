0f4438515843497d20e1c5cacc69f426e9bd5b97
Frostnn <gradysnn92@yandex.ru>
2021-02-26 23:34:47 +0300

    finale

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 import './uploads.js';
-import upload from './uploads.js';
+import { upload } from './uploads.js';
+import firebase from 'firebase/app';
+import 'firebase/storage';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMhd66IcOS1vVqShOeQJYtS-b4FBORYyg',
+  authDomain: 'upload-file-4150d.firebaseapp.com',
+  projectId: 'upload-file-4150d',
+  storageBucket: 'upload-file-4150d.appspot.com',
+  messagingSenderId: '971029319640',
+  appId: '1:971029319640:web:4265a8fb4bce0134c16cad',
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+console.log(storage);
 upload('#file', {
   multi: true,
   accept: ['.png', '.jpg', '.jpeg'],
+  onUpload(files, blocks) {
+    files.forEach((file, i) => {
+      const ref = storage.ref(`images/${file.name}`);
+      const task = ref.put(file);
+      task.on(
+        'state_changed',
+        (snapshot) => {
+          const percentage = (
+            (snapshot.bytesTransferred / snapshot.totalBytes) *
+            100
+          ).toFixed(0);
+          const block = blocks[i].querySelector('.prev_info-progress');
+          block.textContent = percentage + '%';
+          block.style.width = percentage + '%';
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          task.snapshot.ref.getDownloadURL().then((url) => {
+            console.log(url);
+          });
+        }
+      );
+    });
+    console.log('files:', files);
+  },
 });
--- uploads.js
+++ uploads.js
@@ -1,9 +1,36 @@
-const upload = (selector, options = {}) => {
+const bytesToSize = (bytes) => {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (!bytes) {
+    return '0 Byte';
+  }
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
+};
+
+const element = (tag, classes = [], content) => {
+  const node = document.createElement(tag);
+  if (classes.length) {
+    node.classList.add(...classes);
+  }
+
+  if (content) {
+    node.textContent = content;
+  }
+
+  return node;
+};
+
+export const upload = (selector, options = {}) => {
+  const noop = () => {};
+
+  let files = [];
+  const onUpload = options.onUpload ?? noop;
   const input = document.querySelector(selector);
 
-  const open = document.createElement('button');
-  open.classList.add('card_btn');
-  open.textContent = 'Открыть';
+  const prev = element('div', ['prev']);
+  const open = element('button', ['card_btn'], 'Открыть');
+  const uploadFile = element('button', ['card_btn', 'primary'], 'Загрузить');
+  uploadFile.style.display = 'none';
 
   if (options.multi) {
     input.setAttribute('multiple', true);
@@ -13,19 +40,82 @@ const upload = (selector, options = {}) => {
     input.setAttribute('accept', options.accept.join(', '));
   }
 
+  input.insertAdjacentElement('afterend', prev);
+  input.insertAdjacentElement('afterend', uploadFile);
   input.insertAdjacentElement('afterend', open);
+
   const triggerInput = () => {
     input.click();
   };
+
   const changeHandler = (e) => {
     if (!e.target.files.length) {
       return;
     }
 
-    const { files } = e.target;
+    files = Array.from(e.target.files);
+    prev.innerHTML = '';
+    uploadFile.style.display = 'inline';
+
+    files.forEach((item) => {
+      if (!item.type.match('image')) {
+        return;
+      }
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const div = document.createElement('div');
+        div.classList.add('prev_img');
+        const src = e.target.result;
+        div.innerHTML = `
+          <div class="prev_remove" data-name="${item.name}">&times;</div>
+          <img src="${src}" />
+          <div class="prev_info">
+            <span>${item.name}</span>
+            <span>${bytesToSize(item.size)}</span>
+          </div>
+        `;
+        prev.append(div);
+      };
+      reader.readAsDataURL(item);
+    });
+  };
+
+  const removeHeadler = (e) => {
+    if (!e.target.dataset.name) {
+      return;
+    }
+
+    const { name } = e.target.dataset;
+    files = files.filter((file) => file.name != name);
+    if (!files.length) {
+      uploadFile.style.display = 'none';
+    }
+    const block = document
+      .querySelector(`[data-name="${name}"]`)
+      .closest('.prev_img');
+    block.classList.add('removing');
+    setTimeout(() => {
+      block.remove();
+    }, 300);
+  };
+
+  const clearPrev = (elem) => {
+    elem.style.bottom = '4px';
+    elem.innerHTML = '<div class="prev_info-progress"></div>';
+  };
+
+  const uploadHeandler = () => {
+    prev.querySelectorAll('.prev_remove').forEach((item) => {
+      item.remove();
+    });
+    const prevInfo = prev.querySelectorAll('.prev_info');
+    prevInfo.forEach(clearPrev);
+    onUpload(files, prevInfo);
   };
   open.addEventListener('click', triggerInput);
   input.addEventListener('change', changeHandler);
+  prev.addEventListener('click', removeHeadler);
+  uploadFile.addEventListener('click', uploadHeandler);
 };
-
-export default upload;
